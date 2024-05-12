e0631b72da28dba42c7f5c0742a83c7825770c6d
Marcos <marcos510hernandez@gmail.com>
2024-05-12 01:04:27 -0700

    finished exercise 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string,amount) {
+    let i = 0;
+    let phrase = '';
+    while(i < amount){
+        phrase += string;
+        i++;
+    }
+    if(amount < 0){
+        phrase = "ERROR";
+    }
+    return phrase;
+    
 };
 
 // Do not edit below this line
