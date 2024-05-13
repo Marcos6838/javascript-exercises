c22c9cc4667ada493cfaee66bc1167abf4f344ac
Marcos Hernandez <66546838+Marcos6838@users.noreply.github.com>
2024-05-13 00:25:16 -0700

    Add files via upload
    finished exercise 1 through 7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = string.split("");
+    let sizeReverse = reverse.length - 1;
+    let i = 0;
+    let output = "";
+    while(sizeReverse >=0){
+        output+= reverse[sizeReverse];
+        sizeReverse--;
+    }
+    return output;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
-};
+const sumAll = function(start,end) {
+    let output = 0;
+    let temp = 0;
+    if(typeof start !== 'number' || typeof end !== 'number'){
+        return "ERROR";
+    }else{
+        if(start>end){
+            temp = start;
+            start = end;
+            end = temp;
+        }
+        if(start < 0 || end < 0){
+            return "ERROR";
+        }
+        while(start<=end){
+            output = output + start;
+            start++;
+        }
+        return output;
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
